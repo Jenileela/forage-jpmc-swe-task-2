1da4c726cef9cfac0288166abf5da0c8de12ef93
Jenifer <jenileela22@gmail.com>
2024-06-19 20:26:00 +0530

    Made changes in the App.tsx, Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -57,6 +57,17 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // As part of the task, you need to fix the way we update the data props to
       // avoid inserting duplicated entries into Perspective table again.
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('row-pivots', '["timestamp']');
+      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '['aggregates',`
+        {"stock" : "distinct count",
+         "top_ask_price": "avg",
+         "top_bid_price": "avg",
+         "timestamp" : "distinct count"}`);
+
       this.table.update(this.props.data.map((el: any) => {
         // Format the data from ServerRespond to the schema
         return {
